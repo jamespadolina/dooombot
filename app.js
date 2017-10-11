7b2b5274159bac118815dca3e8032256c7b304c8
bflojo <baggycravens@gmail.com>
2017-10-11 16:24:32 -0700

    update for day one. services, controllers, html all created

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module("doomBotApp", ["ui.router"])
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+})
+
+    $stateProvider.state("app", {
+        abstract: true,
+        url: '',
+        templateUrl: './views/app-container.html'
+    })
+
+    .state("app.home", {
+        url: "/",
+        templateUrl: "./views/home.html"
+    })
+
+    .state("app.admin", {
+        url: "/admin",
+        templateUrl: "./views/admin.html",
+        controller: "adminController"
+    })
+
+    .state("app.checkOrder", {
+        url: "/checkOrder",
+        templateUrl: "./views/check-order.html",
+        controller: "checkOrderController"
+    })
+
+    .state("app.orderList", {
+        url: "/orderList",
+        templateUrl: "./views/order-list.html",
+        controller: "orderListController.js"
+    })
