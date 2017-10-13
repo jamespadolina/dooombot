cb12857dbed5c3c539b02b027b792345588b3b56
James <padolinajames@yahoo.com>
2017-10-13 11:05:23 -0700

    something

--- app.js
+++ app.js
@@ -5,35 +5,38 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
 
 
-    $stateProvider.state("home", {
-        url: '/',
+    $stateProvider
+    .state("app", {
+        abstract: true,
+        url: '',
+        templateUrl: './views/app-container.html'
+    })
+
+    .state("app.home", {
+        url: '/home',
         templateUrl: './views/home.html',
         controller: "homeController"
-    })
-
-    .state("orderList", {
-        url: "/orderList",
-        templateUrl: "./views/order-list.html",
-        controller: "orderListController"
-    })
-
-    .state("admin", {
-        url: "/admin",
-        templateUrl: "./views/admin.html",
-        controller: "adminController"
-    })
-
-    .state("checkOrder", {
-        url: "/checkOrder",
-        templateUrl: "./views/check-order.html",
-        controller: "checkOrderController"
     })
 
     .state("app.orderList", {
         url: "/orderList",
         templateUrl: "./views/order-list.html",
-        controller: "orderListController.js"
+        controller: "orderListController"
     })
+
+    .state("app.admin", {
+        url: "/admin",
+        templateUrl: "./views/admin.html",
+        controller: "adminController"
+    })
+
+    .state("app.checkOrder", {
+        url: "/checkOrder",
+        templateUrl: "./views/check-order.html",
+        controller: "checkOrderController"
+    })
+
+   
 
     .state("app.login", {
         url: "/login",
