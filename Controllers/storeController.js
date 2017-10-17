9cb214873c50372380b69a3cc1a34d1c324d8275
bflojo <baggycravens@gmail.com>
2017-10-17 16:17:47 -0700

    merging

--- Controllers/storeController.js
+++ Controllers/storeController.js
@@ -3,28 +3,27 @@ angular
   .controller("storeController", function ($scope, $state, userService) {
 
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
+   // Get the modal
+var modal = document.getElementById('myModal');
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function () {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-    }
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
-   
-      span.onclick = function () {
-        modal.style.display = "none";
-      }
-    
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 
   })
