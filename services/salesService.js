8288fe316146a379c7df894da3ccc8e84e9d7c6d
James <padolinajames@yahoo.com>
2017-10-17 09:42:35 -0700

    pulling

--- services/salesService.js
+++ services/salesService.js
@@ -3,8 +3,7 @@ angular
 .service("salesService", function ($http, $state) {
 
 this.getSales = function() {
-  // return $http.get(`http://192.168.7.75:3000/sales`)
-  return $http.get(`http:/192.168.120.7:3000/sales`)
+  return $http.get(`http://192.168.120.7:3000/sales`)
 }
 
 })
